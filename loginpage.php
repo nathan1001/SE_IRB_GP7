8acb26293623ab9fc88c71c1339aef66a9d539de
unknown <gerrard8donkor@gmail.com>
2016-03-17 22:45:37 +0000

    login first commit

--- loginpage.php
+++ loginpage.php
@@ -1,3 +1,32 @@
 <?php
 
+	//include users.php
+	include_once("projectUsers2.php");
+		//check for user code
+	   if(isset($_REQUEST['usercode'])){
+	   	//create an object of users and delete the user
+		$obj = new projectUsers2();
+
+		$result = $obj->searchUsers($_REQUEST['usercode']);
+		
+		if ($result){
+
+			$result = $obj->fetch();
+			if($username == $result['PASSWORD']){
+
+			//access the page
+			}
+			else{
+				echo "invalid username/password";
+				//redirect to homepage
+				header("Location :userslist.php");
+			}
+
+
+		}
+
+
+	   }
+	  
+
 ?>
