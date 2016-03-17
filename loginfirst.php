11db8318557165dd64ae242e7939d9564a980ed8
unknown <gerrard8donkor@gmail.com>
2016-03-17 23:18:46 +0000

    login second commit

---
+++ loginfirst.php
@@ -0,0 +1,26 @@
+	<html>
+		<head>
+			<title>IRB Login Portal</title>
+			<link rel="stylesheet" href="css/style.css">
+			<script>
+				<!--add validation js script here
+			</script>
+		</head>
+		<body>
+
+		<form>
+						<div id="divPageMenu">
+							<label> USERNAME</label>
+							<input type="text" id="username" name="username"/>
+
+									
+						</div>
+						<div id="divPageMenu">
+							<label> PASSWORD</label>
+							<input type="text" id="password" name="password"/>
+
+									
+						</div>
+						</form>
+		</body>
+	</html>	
