7b2f28686797f44f3c20c2c4d086b30030665c5c
Kobby Annor <cobbyannor@gmail.com>
2016-03-17 21:55:35 +0000

    creating html irb form process2

--- submit.php
+++ submit.php
@@ -1,81 +1,109 @@
 <html>
-<head><title>IRB Application</title></head>
-<body>
+  <head><title>IRB Application</title></head>
+  <body>
   <b>ASHESI UNIVERSITY</b><p>
- <b>University Policy on Human Subjects Research</b><p>
- All research, involving human subjects, which is to be conducted by faculty or students from Ashesi University College must be approved by the Human Subjects Review Committee. This committee is the Institutional Review Board (IRB) of Ashesi University. The purpose of this review is to ensure that the research activity is conducted with the safety of the human subject as its highest priority. The criteria for this review are in strict accord with international standards for the protection of human participants in academic research. All research to be conducted by students, faculty, staff, or research affiliates from other institutions must be approved prior to primary data collection or project initiation.
-Human Subjects approval must precede submission of all research applications to any sponsor for funding of the research effort. This applies to funding requests for internal or external resources, domestic and international applications. This policy applies to all applications including those to for-profit, not-for-profit, governmental or charitable organizations.
-The committee will respond to each proposal within four weeks following its review. If the Committee determines that a submission involves vulnerable populations or greater than minimal risk, the applicant will have an opportunity meet with the Committee to address issues of concern. The applicant will have an opportunity to modify the research design, sampling strategies, process or format of data collection, or any other concern that is raised by the Committee. Appeals can be formally addressed by the Provost with the Chair of the Human Subjects Review Committee. If an appeal is approved, the full committee will revisit the proposal and will entertain all concerns of the Principal Investigator. In the event that an appeal is also denied, the proposal must be closed for further consideration unless methodological and ethical issues are adequately addressed in the form of a new submission.
-All submissions must be in digital form. The submission must be approved with signature from the Committee Chair. Submissions are accepted on an ongoing basis.
-All submissions must be made using the Ashesi University Human Subjects Review Form. The form is found on the following pages and is also available at: website. Applications that are incomplete will not be forwarded to the Committee for review. Incomplete applications will be returned to the applicant. Applicants are advised to be mindful of schedules and time requirements for the review process and urged to submit proposals as early as possible.<p>
+  <b>University Policy on Human Subjects Research</b><p>
+  All research, involving human subjects, which is to be conducted by faculty or students from Ashesi University College must be approved by the Human Subjects Review Committee. This committee is the Institutional Review Board (IRB) of Ashesi University. The purpose of this review is to ensure that the research activity is conducted with the safety of the human subject as its highest priority. The criteria for this review are in strict accord with international standards for the protection of human participants in academic research. All research to be conducted by students, faculty, staff, or research affiliates from other institutions must be approved prior to primary data collection or project initiation.
+  Human Subjects approval must precede submission of all research applications to any sponsor for funding of the research effort. This applies to funding requests for internal or external resources, domestic and international applications. This policy applies to all applications including those to for-profit, not-for-profit, governmental or charitable organizations.
+  The committee will respond to each proposal within four weeks following its review. If the Committee determines that a submission involves vulnerable populations or greater than minimal risk, the applicant will have an opportunity meet with the Committee to address issues of concern. The applicant will have an opportunity to modify the research design, sampling strategies, process or format of data collection, or any other concern that is raised by the Committee. Appeals can be formally addressed by the Provost with the Chair of the Human Subjects Review Committee. If an appeal is approved, the full committee will revisit the proposal and will entertain all concerns of the Principal Investigator. In the event that an appeal is also denied, the proposal must be closed for further consideration unless methodological and ethical issues are adequately addressed in the form of a new submission.
+  All submissions must be in digital form. The submission must be approved with signature from the Committee Chair. Submissions are accepted on an ongoing basis.
+  All submissions must be made using the Ashesi University Human Subjects Review Form. The form is found on the following pages and is also available at: website. Applications that are incomplete will not be forwarded to the Committee for review. Incomplete applications will be returned to the applicant. Applicants are advised to be mindful of schedules and time requirements for the review process and urged to submit proposals as early as possible.<p>
   <b>Request for Human Subjects Approval</b><p>
-   <b>________________________________________</b><p>
-
-
-<b>General Research Application Form</b><p>
-<b>Use this form for general research only. It is not for health or medical research
-Use this form for initial approvals and protocol modifications. To renew an approval after one year, please use the Continuation Form.
-Submit this completed form and your proposal with all required elements as email attachments to the office of the Provost, Ashesi University College: (irb@ashesi.edu.gh).
-Also, send one hard copy of signed original approval form with a complete copy of your research proposal and all required elements to: Human Subjects Review Committee, Ashesi University, Berekuso, E.R., Ghana.
-</b><p>
- <b>________________________________________</b><p>
-
-<form action="add.php" method="GET">
-	Title of Project: <input type="text" name="username" maxlength="10"/><p>
-
+  <b>________________________________________</b><p>
+  
+  
+  <b>General Research Application Form</b><p>
+  <b>Use this form for general research only. It is not for health or medical research
+  Use this form for initial approvals and protocol modifications. To renew an approval after one year, please use the Continuation Form.
+  Submit this completed form and your proposal with all required elements as email attachments to the office of the Provost, Ashesi University College: (irb@ashesi.edu.gh).
+  Also, send one hard copy of signed original approval form with a complete copy of your research proposal and all required elements to: Human Subjects Review Committee, Ashesi University, Berekuso, E.R., Ghana.
+  </b><p>
+  <b>____________________________________________________________________________________</b><p>
+  
+  <form action="add.php" method="GET">
+  Title of Project: <input type="text" name="username" maxlength="10"/><p>
+  
   Principal Investigator: <input type="text" name="username" maxlength="10"/><p>
-Co-Principal Investigator(s):<input type="text" name="username" maxlength="10"/><p>
-
-Principal Investigator Department <input type="text" name="username" maxlength="10"/><p>
-Principal Investigator Phone <input type="text" name="username" maxlength="10"/><p>
-Email<input type="text" name="username" maxlength="10"/><p>
- Fax <input type="text" name="username" maxlength="10"/><p>
-	<!-- UserGroup:<select name="usergroup">
-<option value="1">Admin</option>
-<option value="2">Faculty</option>
-<option value="3">Students</option>
-</select> -->
+  Co-Principal Investigator(s):<input type="text" name="username" maxlength="10"/><p>
+  
+  Principal Investigator Department <input type="text" name="username" maxlength="10"/><p>
+  Principal Investigator Phone <input type="text" name="username" maxlength="10"/><p>
+  Email<input type="text" name="username" maxlength="10"/>
+  Fax <input type="text" name="username" maxlength="10"/><p>
+If an external grant is being sought, or already approved for this project, state the funding source and submission deadline or date of project initiation
 <p>
-<input type="submit" value="submit"/>
+  Grant:<input type="text" name="username" maxlength="10"/><p>
 
-</form>
+  <b>General Instructions</b><p>
+This application form must be submitted with the research proposal, consent forms to be used with subjects in the research, questionnaires and vernacular translations if appropriate, and any additional documents that will assist reviewers to fully understand the purposes, methods, and field procedures that will be used. Please complete every section of this application form. If any section is not relevant to your research indicate with “N/A” or write “Not Applicable”.
+Please do not attempt to squeeze answers into the spaces provided on this form. It is produced in M.S. Word and you can enter text below each question. Be sure to review your application before submission to make certain that it is complete. If it is not complete a decision about the application will be delayed. If you have a question about this application, please email your question to irb@asehsi.edu.gh
+<p>
+  <b>I. If you are requesting an exemption from Human Subjects Review Committee (HSRC) review, 
+    explain the basis for the requested exemption (see attached list of exempt project types):</b><p>
+Click here to enter text:<input type="text" name="username" size="100" maxlength="100"/><p>
+ <b>II. Numbers, Types and Recruitment of Subjects </b><p>
+A. Identify the numbers and characteristics of subjects (e.g., age ranges, sex, ethnic background, health status, disabilities, etc.).
+Click here to enter text
+B. Special Classes. If applicable, explain the rationale for the use of special classes or subjects such as pregnant women, children, prisoners, mentally impaired, institutionalized, or others who are likely to be particularly vulnerable.
+Click here to enter text.
+C. How are the individual participants to be recruited for this research? Is it clear to the subjects that participation is voluntary and that they may withdraw at any time without negative consequences?
+Click here to enter text.
+D. To what extent and how are the subjects to be informed of research procedures before their participation?
+Click here to enter text.
+E. Attach a copy of the written "Informed Consent” form or a written statement of the oral consent. If this is produced in vernacular languages please provide a copy in each language being used in field work. The consent form should include the purpose of the research, that the engagement is voluntary, duration of engagement with the subject, risk and benefit, contact information. See template below
 
-</body>
-</html>
-<?php
 
-if(!isset($_REQUEST["username"])){
+  <!-- UserGroup:<select name="usergroup">
+  <option value="1">Admin</option>
+  <option value="2">Faculty</option>
+  <option value="3">Students</option>
+  </select> -->
 
-exit();
 
+
+
+
+  <p>
+  <input type="submit" value="submit"/>
+  
+  </form>
+  
+  </body>
+  </html>
+  <?php
+  
+  if(!isset($_REQUEST["username"])){
+  
+  exit();
+  
 }
-
-$username=$_REQUEST["username"];
-$usergroup=$_REQUEST["usergroup"];
-$db=new mysqli('127.0.0.1','root','9144','test');
-if($db->connect_errno){
-
-	exit();
-}
-else{
-
-	//echo "connected!";
-}
-
-$sql="insert into user set 
-username='$username',
- FIRSTNAME='none',
-        LASTNAME='none',
-        PWORD=MD5('none'),
-        USERGROUP='$usergroup'";
-
-;
-
+  
+  $username=$_REQUEST["username"];
+  $usergroup=$_REQUEST["usergroup"];
+  $db=new mysqli('127.0.0.1','root','9144','test');
+  if($db->connect_errno){
+    
+    exit();
+  }
+  else{
+    
+    //echo "connected!";
+  }
+  
+  $sql="insert into user set 
+    username='$username',
+    FIRSTNAME='none',
+    LASTNAME='none',
+    PWORD=MD5('none'),
+    USERGROUP='$usergroup'";
+    
+    ;
+  
   if($db->query($sql)){
     echo "Data Added";
   }else{
     echo "Error while adding data";
   }
   
-
-?>
+  
+  ?>
