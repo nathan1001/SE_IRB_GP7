60a0f36e4829541f8d56f459a0782a7514403f37
unknown <mahesh.moolchandani@ashesi.edu.gh>
2016-03-18 10:48:37 +0000

    edit.php v.2.

--- edit.php
+++ edit.php
@@ -1,12 +1,11 @@
 <?php
 /**
 */
-include_once("adb.php");
-include_once("upload.php")
+include_once("projectuser2.php")
 /**
 *edit  class
 */
-class edit extends adb{
+class edit extends projectuser2{
 	function edit(){
 	}
 	/**
@@ -19,6 +18,14 @@ class edit extends adb{
 	*@param string principalInvestigatorEmail applicant's email
 	*@param int principalInvestigatorFax applicant's fax
 	*@param string externalGrant  information on external grant if sought
+	*@param string hsrcExemption reasons project should be excluded from HSRC
+	*@param string demographicA the numbers and characteristics of subjects (e.g., age ranges, sex, ethnic background, health status, disabilities, etc.)
+	*@param string demographicB special classes for participants
+	*@param string demographicD how informed the participants are
+	*@param BLOB informedConsent informed consent from participants
+	*@param string demographicF classification  and specification for research methods
+	*@param string demographicG description of data sources used in research
+	*@param array
 	*@return boolean returns true if successful or false 
 	*/
 	function save($projectTitle,$principalInvestigator='none',$coprincipalInvestigators='none',$principalInvestigatorDept='none',$principalInvestigatorPhone,$principalInvestigatorEmail,$principalInvestigatorFax,$externalGrant,$hsrcExemption,$demographicA,$demographicB,$demographicC,$demographicD,$informedConsent,$demographicF,$demographicG,$Risk,$RiskYes,$ConfidentialityA,$ConfidentialityB,$ConfidentialityCi,$ConfidentialityCii,$ConfidentialityCiii,$BenefitsA,$BenefitsB,$reserachProposal,$researchProposalNo,$summary,$consentAgreements,$instruments,$flyers,$exclusion,$extension){
@@ -59,3 +66,9 @@ class edit extends adb{
 
 		return $this->query($strQuery);				
 	}
+}
+function openApplication($incompleteID){
+	$strQuery= "select * from incomplete where incompleteID='incompleteID'";
+	return $this->query($strQuery);
+	}
+}
