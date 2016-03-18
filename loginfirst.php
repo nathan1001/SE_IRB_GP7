c966c72a8f96f4592113b64c07bb8d3cf1dc6601
unknown <gerrard8donkor@gmail.com>
2016-03-18 00:57:48 +0000

    login second commit

--- loginfirst.php
+++ loginfirst.php
@@ -21,6 +21,41 @@
 
 									
 						</div>
+						<input type="submit" value="login" name="login" >
 						</form>
 		</body>
 	</html>	
+		<?php
+	include_once("projectUsers2.php");
+		//1) Create object of users class
+		$obj = new projectUsers();
+		//2) Call the object's getUsers method and check for error	
+		if(isset($_REQUEST['login']) ) {
+				//check for user code
+	   if(isset($_REQUEST['usercode'])){
+	   	//create an object of users and delete the user
+		$obj = new projectUsers();
+
+		$result = $obj->searchUsers($_REQUEST['username']);
+		
+		if ($result){
+
+			$result = $obj->fetch();
+			if($_REQUEST['password'] == $result['PASSWORD']){
+
+			//access the page
+			}
+			else{
+				echo "invalid username/password";
+				//redirect to homepage
+				return;
+			}
+
+
+		}
+
+
+	   }
+			
+
+		}
