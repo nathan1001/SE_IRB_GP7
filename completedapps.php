ebda3e575e30949548d721b0f6be676cde8eaaf2
Kobby Annor <cobbyannor@gmail.com>
2016-04-22 17:15:54 +0000

    code format

--- completedapps.php
+++ completedapps.php
@@ -1,23 +1,23 @@
 <?php
-  /**
+   /**
+    */
+   include_once("adb.php");
+   /**
+   *completedapps  class with necessaray function to submit a completed form
    */
-  include_once("adb.php");
-/**
- *completedapps  class
- */
-class completedapps extends adb{
-  function completedapps(){
-  }
-  /**
-   *submits a completed IRB application
-   *@param string ProjectTitle login name
-   *@return boolean returns true if successful or false 
-   */
-  function submitForm($ProjectTitle){
-    $strQuery="insert into completed set
-      ProjectTitle='$ProjectTitle'
-      ";
-      return $this->query($strQuery);    
-  }
-}
-?> 
+   class completedapps extends adb{
+   function completedapps(){
+   }
+   /**
+    *submits a completed IRB application
+    *@param string ProjectTitle login name
+    *@return boolean returns true if successful or false 
+    */
+   function submitForm($ProjectTitle){
+     $strQuery="insert into completed set
+       ProjectTitle='$ProjectTitle'
+       ";
+       return $this->query($strQuery);    
+   }
+   }
+   ?>
